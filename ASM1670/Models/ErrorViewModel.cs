2bbe3a5eb9f0122b8418a7547f685beaf9529156
hunglv2001 <levinhhung20@gmail.com>
2023-04-20 21:36:37 +0700

    Add ApplicationUser Model

---
+++ ASM1670/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASM1670.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
